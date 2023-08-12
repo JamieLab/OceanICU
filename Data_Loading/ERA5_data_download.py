9b3cbac227a3f917d8d77a6584ffa37ce7cece84
Daniel Ford <77839711+danielford166@users.noreply.github.com>
2023-08-12 19:38:28 +0100

    Update ERA5_data_download.py
    Condensing code

--- Data_Loading/ERA5_data_download.py
+++ Data_Loading/ERA5_data_download.py
@@ -46,57 +46,34 @@ def era5_average(loc,outloc,start_yr=1990,end_yr=2023,log=[],lag=[],var=None,org
     du.makefolder(outloc)
     res = np.round(np.abs(log[0]-log[1]),2)
 
-    if res > orgi_res:
-        # If we are averaging to a 1 deg grid for example then we use the grid averaging.
-        # However if the new grid is higher resolution then we need to spatially interpolate.
-        yr = start_yr
-        mon = 1
-        t = 0
-        while yr < end_yr:
-            if mon == 1:
-                du.makefolder(os.path.join(outloc,str(yr)))
-            file = os.path.join(loc,str(yr),str(yr)+'_'+du.numstr(mon)+'_ERA5.nc')
-            outfile = os.path.join(outloc,str(yr),str(yr)+'_'+du.numstr(mon)+f'_ERA5_{var}_'+str(res)+'_deg.nc')
-            if du.checkfileexist(file) and not du.checkfileexist(outfile):
-                if t == 0:
-                    lon,lat = du.load_grid(file)
+    yr = start_yr
+    mon = 1
+    t = 0
+    while yr < end_yr:
+        if mon == 1:
+            du.makefolder(os.path.join(outloc,str(yr)))
+        file = os.path.join(loc,str(yr),str(yr)+'_'+du.numstr(mon)+'_ERA5.nc')
+        outfile = os.path.join(outloc,str(yr),str(yr)+'_'+du.numstr(mon)+f'_ERA5_{var}_'+str(res)+'_deg.nc')
+        if du.checkfileexist(file) and not du.checkfileexist(outfile):
+            if t == 0:
+                lon,lat = du.load_grid(file)
 
-                c = Dataset(file,'r')
-                va_da = np.transpose(np.squeeze(np.array(c.variables[var][:])))
-                c.close()
-                lon,va_da=du.grid_switch(lon,va_da)
-
+            c = Dataset(file,'r')
+            va_da = np.transpose(np.squeeze(np.array(c.variables[var][:])))
+            c.close()
+            lon,va_da=du.grid_switch(lon,va_da)
+            
+            if res > orgi_res:
+                # If we are averaging to a 1 deg grid for example then we use the grid averaging.
+                # However if the new grid is higher resolution then we need to spatially interpolate.
                 if t == 0:
                     lo_grid,la_grid = du.determine_grid_average(lon,lat,log,lag)
                     t = 1
                 va_da_out = du.grid_average(va_da,lo_grid,la_grid)
-                du.netcdf_create_basic(outfile,va_da_out,var,lag,log)
-            mon = mon+1
-            if mon == 13:
-                yr = yr+1
-                mon=1
-    else:
-        # We need to interpolate the values instead :)
-        yr = start_yr
-        mon = 1
-        t = 0
-        while yr < end_yr:
-            if mon == 1:
-                du.makefolder(os.path.join(outloc,str(yr)))
-            file = os.path.join(loc,str(yr),str(yr)+'_'+du.numstr(mon)+'_ERA5.nc')
-            outfile = os.path.join(outloc,str(yr),str(yr)+'_'+du.numstr(mon)+f'_ERA5_{var}_'+str(res)+'_deg.nc')
-            if du.checkfileexist(file) and not du.checkfileexist(outfile):
-                if t == 0:
-                    lon,lat = du.load_grid(file)
-
-                c = Dataset(file,'r')
-                va_da = np.transpose(np.squeeze(np.array(c.variables[var][:])))
-                c.close()
-                lon,va_da=du.grid_switch(lon,va_da)
+            else:
                 va_da_out = du.grid_interp(lon,lat,va_da,log,lag)
-
-                du.netcdf_create_basic(outfile,va_da_out,var,lag,log)
-            mon = mon+1
-            if mon == 13:
-                yr = yr+1
-                mon=1
+            du.netcdf_create_basic(outfile,va_da_out,var,lag,log)
+        mon = mon+1
+        if mon == 13:
+            yr = yr+1
+            mon=1
